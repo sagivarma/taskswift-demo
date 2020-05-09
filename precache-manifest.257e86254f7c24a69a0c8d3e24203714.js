f0f0517edf7632935a9a041ba291274f93576821
sagivarma <sagivarma005@gmail.com>
2020-05-09 21:09:41 +0530

    Updates

---
+++ precache-manifest.257e86254f7c24a69a0c8d3e24203714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe089bef0c89aa1c2f98ca65ee61017",
+    "url": "/taskswift-demo/index.html"
+  },
+  {
+    "revision": "36c0366195dc1a56815d",
+    "url": "/taskswift-demo/static/css/main.31187e73.chunk.css"
+  },
+  {
+    "revision": "7cdb78b4d911c4f2b257",
+    "url": "/taskswift-demo/static/js/2.c3ac11c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taskswift-demo/static/js/2.c3ac11c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c0366195dc1a56815d",
+    "url": "/taskswift-demo/static/js/main.ab03a644.chunk.js"
+  },
+  {
+    "revision": "c74e5c74a2f862981f06",
+    "url": "/taskswift-demo/static/js/runtime-main.7caa40b8.js"
+  }
+]);
